cfeda673842be178c5b1ea7aa8742434977397a5
Mason Du <mdu@benbria.com>
2012-04-14 11:44:21 -0400

    dev server coffee script

--- web/server.js
+++ web/server.js
@@ -3,10 +3,15 @@
  * Module dependencies.
  */
 
+require('coffee-script');
+
 var express = require('express')
-  , routes = require('./routes');
+  , RedisStore = require('connect-redis')(express);
+
+  var routes = require('./routes');
 
 var app = module.exports = express.createServer();
+
 
 // Configuration
 
@@ -29,7 +34,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+require('./routes/index')(app)
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
