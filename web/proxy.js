4d2a80a3efee83019f05253ff056a2d0c14f8cfe
Rob Spectre <rob.spectre@gmail.com>
2012-04-14 16:40:59 -0400

    Trying proxy.

--- web/proxy.js
+++ web/proxy.js
@@ -4,26 +4,21 @@ var http = require('http'),
     https = require('https'),
     httpProxy = require('http-proxy');
 
-function middleware(res, callback) {
-    var _end = res.end;
-    res.end = function(data) {
-        _end.call(res, data);
-        if (callback) callback();
-    }
-}
-
-httpProxy.createServer({
+var server = httpProxy.createServer({
     target: { host: 'graph.facebook.com', port: 443, https: true },
     changeOrigin: true },
     function (req, res, proxy) {
         console.log("Received request.");
-        var buffer = httpProxy.buffer(req);
-        res.on('data', function(data) {
-            console.log(data);
-        });
-       // middleware(res, function() {
-       //     console.log("The request was proxied");
-       // });
-        proxy.proxyRequest(req, res, buffer);
-}).listen(8000);
+        server.proxy.response_buffer = '';
+        proxy.proxyRequest(req, res);
+});
 
+server.proxy.on('data', function(data) {
+    server.proxy.response_buffer += data;
+});
+
+server.proxy.on('end', function() {
+    console.log(server.proxy.response_buffer.toString('utf-8'));
+});
+
+server.listen(8000);
