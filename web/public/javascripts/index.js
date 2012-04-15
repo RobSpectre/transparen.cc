142399b664887b86053464c79e758f63e873de4c
Mason Du <mdu@benbria.com>
2012-04-15 13:55:03 -0400

    added link on index page. changed websocket hostname for nodejitsu hosting

--- web/public/javascripts/index.js
+++ web/public/javascripts/index.js
@@ -1,4 +1,3 @@
-var r = Raphael(10, 50, 640, 480);
 
 var pieval = [1,1];
 
@@ -9,7 +8,7 @@ $('document').ready(function() {
       
 
       $("td.timeago").timeago();
-      var socket = io.connect('http://localhost:3000');
+      var socket = io.connect('http://tcc.nodejitsu.com');
      
       $("td.timeago").timeago();
 
@@ -38,27 +37,3 @@ $('document').ready(function() {
 
        
 
-function setupPie(piedata) {
-  r.clear();
-  // Creates pie chart at with center at 320, 200,
-  // radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
-  var pie = r.piechart(320, 240, 100, piedata.slice());
-
-  pie.hover(function () {
-    this.sector.stop();
-    this.sector.scale(1.1, 1.1, this.cx, this.cy);
-
-    if (this.label) {
-      this.label[0].stop();
-      this.label[0].attr({ r: 7.5 });
-      this.label[1].attr({ "font-weight": 800 });
-    }
-  }, function () {
-    this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-    if (this.label) {
-      this.label[0].animate({ r: 5 }, 500, "bounce");
-      this.label[1].attr({ "font-weight": 400 });
-    }
-
-  });
-}
